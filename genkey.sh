8f64998bd9e1376d2f9b8ce45da337dec2d38b50
Ming Cheng <mingcheng@users.noreply.github.com>
2019-07-03 12:21:44 +0800

    modify for raspberry prod env

--- genkey.sh
+++ genkey.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 \
-    -out cert.pem -subj "/C=CN/ST=ZJ/L=HZ/O=NetEase/OU=DEV/CN=163.com/emailAddress=hzmingcheng@crop.netease.com"
+openssl req -newkey rsa:2048 -nodes -x509 -days 365 \
+	-keyout key.pem -out cert.pem \
+	-subj "/C=CN/ST=ZJ/L=HZ/O=NetEase/OU=DEV/CN=163.com/emailAddress=hzmingcheng@crop.netease.com"
+
